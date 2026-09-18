50d6c4add191a294ffc1a550a0d550b52ff7cff0
TifloAcosta <146574974+TifloAcosta@users.noreply.github.com>
2026-09-18 20:31:34 +0100

    config: keep advanced analyzer disabled until deployment

--- download-config.js
+++ download-config.js
@@ -1,3 +1,4 @@
 window.TIFLO_DOWNLOAD_CONFIG = Object.freeze({
-  endpoint: 'https://download.tifloacosta.com/analyze'
+  endpoint: '',
+  plannedEndpoint: 'https://download.tifloacosta.com/analyze'
 });

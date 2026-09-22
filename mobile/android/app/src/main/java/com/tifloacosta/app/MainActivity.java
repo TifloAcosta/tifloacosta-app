1b05669c4ec9c305a4eb876df827f81fd4a4a4ec
TifloAcosta <146574974+TifloAcosta@users.noreply.github.com>
2026-09-22 13:05:15 +0100

    feat: register Android share plugin

--- mobile/android/app/src/main/java/com/tifloacosta/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/tifloacosta/app/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(TifloSavePlugin.class);
+        registerPlugin(TifloSharePlugin.class);
         super.onCreate(savedInstanceState);
     }
 }

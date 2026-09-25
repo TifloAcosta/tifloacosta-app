9905765a6d830e8453503c9f010ff82ed173fdb8
TifloAcosta <146574974+TifloAcosta@users.noreply.github.com>
2026-09-25 16:14:23 +0100

    feat: check Play updates on Android lifecycle

--- mobile/android/app/src/main/java/com/tifloacosta/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/tifloacosta/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private TifloUpdatePromptController updatePromptController;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(TifloSavePlugin.class);
@@ -12,5 +14,24 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(TifloWebFetchPlugin.class);
         registerPlugin(TifloUpdatePlugin.class);
         super.onCreate(savedInstanceState);
+        updatePromptController = new TifloUpdatePromptController(this);
+        updatePromptController.start();
+        updatePromptController.checkForUpdate();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (updatePromptController != null) {
+            updatePromptController.checkForUpdate();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (updatePromptController != null) {
+            updatePromptController.stop();
+        }
+        super.onDestroy();
     }
 }
